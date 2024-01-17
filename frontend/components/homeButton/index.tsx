0c2022e7273ac57a784b8552bea6e0473ce974fb
Batuhan Erden <erden.batuhan@gmail.com>
2024-01-18 00:11:48 +0100

    add home buttons

---
+++ frontend/components/homeButton/index.tsx
@@ -0,0 +1,33 @@
+import { ReactElement, cloneElement } from "react";
+import {
+  Card,
+  Typography,
+  CardActionArea
+} from "@mui/material";
+
+import styles from "./homeButton.module.css";
+
+type HomeButtonPropsType = {
+  title: string;
+  iconElement: ReactElement;
+}
+
+const HomeButton = ({ title, iconElement }: HomeButtonPropsType) => {
+  return (
+    <Card className={styles.buttonCard}>
+      <CardActionArea className={styles.buttonActionArea}>
+        <Typography className={styles.buttonIconWrapper}>
+          {cloneElement(iconElement, {
+            className: styles.buttonIcon
+          })}
+        </Typography>
+
+        <Typography className={styles.buttonTextWrapper} variant="h5">
+          {title}
+        </Typography>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default HomeButton;
--- frontend/pages/home/index.tsx
+++ frontend/pages/home/index.tsx
@@ -1,29 +1,32 @@
-import * as React from "react";
 import { NextPage } from "next";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import { Button } from "@mui/material";
+import { Box, Grid } from "@mui/material";
+import LibraryBooksOutlinedIcon from "@mui/icons-material/LibraryBooksOutlined";
+import SupportAgentOutlinedIcon from "@mui/icons-material/SupportAgentOutlined";
+import LightbulbOutlinedIcon from "@mui/icons-material/LightbulbOutlined";
+
+import HomeButton from "../../components/homeButton";
 
 const Home: NextPage = () => {
-
-  const handleClick = async () => {
-    console.log("clicked");
-  };
-
   return (
     <>
       <Box sx={{ flexGrow: 1, mt: 15, mb: 15 }}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={handleClick}
-                sx={{ width: "50%", height: "4rem", fontSize: "1.2rem" }}
-              >
-                Some other Button
-              </Button>
+            <Box sx={{ display: "flex", justifyContent: "center", gap: "1em" }}>
+              <HomeButton
+                title="Knowledge Base"
+                iconElement={<LibraryBooksOutlinedIcon />}
+              />
+
+              <HomeButton
+                title="Tickets"
+                iconElement={<SupportAgentOutlinedIcon />}
+              />
+
+              <HomeButton
+                title="FAQ Insights"
+                iconElement={<LightbulbOutlinedIcon />}
+              />
             </Box>
           </Grid>
         </Grid>
